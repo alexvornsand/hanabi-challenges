a9fb8b51ede4f82aef1519c8d7ea95a7c176c72c
alexvornsand <alexvornsand@gmail.com>
2026-04-24 10:49:14 -0500

    chore: monorepo scaffold with dsl, server, web packages

---
+++ packages/web/src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <p>Hanabi Platform</p>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
